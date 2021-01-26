3141abf986f0431492bd671750db471769467ccd
pow <21pvachirajindakul@athenian.org>
2021-01-26 09:06:07 -0800

    done

--- divisibility_check.py
+++ divisibility_check.py
@@ -1,3 +1,11 @@
-# Collaborators: 
-#
+# Collaborators:
+
+
+for x in range(2, 51):
+    if(x % 3 == 0):
+        print(f'{x} is divisible by 3')
+    else:
+        print(f'{x} is not divisible by 3')
+
+
 
